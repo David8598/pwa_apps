d4839bfb2794edbb183c8737847cf9fd993c0b71
arturious9090 <sefer07artur@gmail.com>
2024-12-13 22:15:01 +0700

    Start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react"
+import "./App.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <Button>
+        Button 
+      </Button>
+    </div>
+  )
+}
+
+export default App
